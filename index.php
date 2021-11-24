92e9f361dba125b32935e762fe767a76c9d12f38
Maximersb <94387211+Maximersb@users.noreply.github.com>
2021-11-24 14:14:20 +0100

    Update index.php
    Index finito

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -40,4 +41,52 @@
     </div>
 </main>
 </body>
+=======
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="preconnect" href="//fonts.googleapis.com">
+    <link rel="preconnect" href="//fonts.gstatic.com" crossorigin>
+    <link href="//fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>Main</title>
+</head>
+<body>
+<header>
+    <img src="" alt=""> <!-- Les 4 points -->
+    <nav>
+        <a href="">Button 1</a>
+        <a href="">Button 2</a>
+        <a href="">Button 3</a>
+        <a href="">Button 4</a>
+    </nav>
+</header>
+<main>
+    <label for="search_bar">Search Bar</label>
+    <input type="text" id="search_bar" name="search_bar">
+
+    <div class="recommendation">
+        <img src="" alt=""> <!-- Film recommendation 1 -->
+        <a href="a"></a> <!-- Button visionage film-->
+        <img src="" alt=""> <!-- Film recommendation 2 -->
+        <a href=""></a> <!-- Button visionage film-->
+        <img src="" alt=""> <!-- Film recommendation 3 -->
+        <a href=""></a> <!-- Button visionage film-->
+        <img src="" alt=""> <!-- Film recommendation 4 -->
+        <a href=""></a> <!-- Button visionage film-->
+    </div>
+
+    <div class="account">
+        <a href="">Connection</a>
+        <a href="">Inscription</a>
+        <a href="">Student</a>
+        <a href="">Normal</a>
+        <a href="">Family</a>
+    </div>
+</main>
+</body>
+>>>>>>> Stashed changes
 </html>
