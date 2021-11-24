fe8997e024fe9adb648db4a0fc04d6b5cdcb86d1
Maxime <maxime.brossard5@gmail.com>
2021-11-24 16:47:04 +0100

    Auto stash before merge of "dev" and "origin/dev"

---
+++ chooseProfil.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="preconnect" href="//fonts.googleapis.com">
+    <link rel="preconnect" href="//fonts.gstatic.com" crossorigin>
+    <link href="//fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <title>IUT-Hub</title>
+
+    <link rel = "icon" href ="/img/logo_iuthub.png" type = "image/x-icon">
+</head>
+<body class="chooseProfile">
+<img id="imgBackgroundP2" src="img/part-2.svg">
+<img id="imgBackgroundP1" src="img/part-1.svg">
+
+<h2 id="titleWho">Qui est-ce ?</h2>
+</body>
