afe19ff3b22660c90a6d388f5fd4b9bb21ad39b9
lolo-pixel <lo.perrian@gmail.com>
2021-11-24 15:30:49 +0100

    temporary login

--- modules/header.php
+++ modules/header.php
@@ -1,9 +1,14 @@
 <header>
-    <img src="" alt=""> <!-- Les 4 points -->
-    <nav>
-        <a href="">Button 1</a>
-        <a href="">Button 2</a>
-        <a href="">Button 3</a>
-        <a href="">Button 4</a>
-    </nav>
+	<img src="" alt=""> <!-- Les 4 points -->
+	<nav>
+		<a href="">Button 1</a>
+		<a href="">Button 2</a>
+		<a href="">Button 3</a>
+		<a href="">Button 4</a>
+	</nav>
+	<?php 
+		if(!empty($_SESSION["admin"])){
+			echo "welcome ". $_SESSION["user"];
+		}
+	?>
 </header>
