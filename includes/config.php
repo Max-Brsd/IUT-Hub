6f1e58781e47f3c562cea172405499ed54080488
lolo-pixel <lo.perrian@gmail.com>
2021-11-24 14:49:30 +0100

    base php

---
+++ includes/config.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+define("ADRESSE", "71 Rue Peter Fink, 01000, Bourg-en-Bresse");
---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+require_once __DIR__."/includes/config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Comfortaa" />
+		<link rel="icon" type="image/png" href="favicon.ico" />
+		<title>Accueil</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<?php include 'modules/header.php'; ?>
+		<main>
+			<form id="form-login" method="POST" enctype="multipart/form-data" action="login.php">
+				<fieldset>
+					<?php
+						if(isset($_SESSION['error']) && $_SESSION['error']){
+							unset($_SESSION['error']);
+							echo "<a>Error in the login, password or mail incorrect</a>";
+						}
+					 ?>
+					<legend>Login</legend>
+					<label >Identifiant</label>
+					<input id="email" type="text" name="email" required>
+					<label>Mot de passe</label>
+					<input id="password" type="password" name="password" required>
+					<button type="submit">Valider</button>
+				</fieldset>
+			</form>
+		</main>
+		<script src="js/script.js"></script>
+	</body>
+</html>
