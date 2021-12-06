82e3a065d25109cca350abe3b0aa173e52097e91
Maximersb <maxime.raffaelli13@gmail.com>
2021-12-06 15:57:25 +0100

    hey

--- modules/header.php
+++ modules/header.php
@@ -1,6 +1,6 @@
 <header>
 
-
+    <div class="navbar">
     <input class="burger" type="checkbox">
     <img class="logo-burger" src="img/menu-icon.svg" alt="Logo menu burger">
 
@@ -11,16 +11,12 @@
         <a href=""><li>Normal</li></a>
         <a href=""><li>Family</li></a>
     </ul>
+    </div>
 
     <span class="logo"><img src=<?= '"img/logo_iuthub.png'.webp().'"' ?>  alt=""></span>
 
 
-	<nav>
-		<a href="">Button 1</a>
-		<a href="">Button 2</a>
-		<a href="">Button 3</a>
-		<a href="">Button 4</a>
-	</nav>
+
 	<?php 
 		if(!empty($_SESSION["admin"])){
 			echo "welcome ". $_SESSION["user"];
