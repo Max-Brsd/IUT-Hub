bd9b2e35c72d0a06273fcf77a12a050ea3d5d6f4
lolo-pixel <lo.perrian@gmail.com>
2021-12-06 17:33:36 +0100

    fixing

--- modules/header.php
+++ modules/header.php
@@ -1,19 +1,19 @@
 <header>
 
-    <div class="navbar">
-    <input class="burger" type="checkbox">
-    <img class="logo-burger" src="img/menu-icon.svg" alt="Logo menu burger">
+	<div class="navbar">
+	<input class="burger" type="checkbox">
+	<img class="logo-burger" src="img/menu-icon.svg" alt="Logo menu burger">
 
-    <ul id="menu">
-        <a href=""><li>Connection</li></a>
-        <a href=""><li>Inscription</li></a>
-        <a href=""><li>Student</li></a>
-        <a href=""><li>Normal</li></a>
-        <a href=""><li>Family</li></a>
-    </ul>
-    </div>
+	<ul id="menu">
+		<a href=""><li>Connection</li></a>
+		<a href=""><li>Inscription</li></a>
+		<a href=""><li>Student</li></a>
+		<a href=""><li>Normal</li></a>
+		<a href=""><li>Family</li></a>
+	</ul>
+	</div>
 
-    <span class="logo"><img src="img/logo_iuthub2.png"  alt=""></span>
+	<span class="logo"><img src="img/logo_iuthub2.png"  alt=""></span>
 
 
 
