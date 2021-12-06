16d5e99f018edb3235dc6503887557474d736922
lolo-pixel <lo.perrian@gmail.com>
2021-12-06 14:51:55 +0100

    create database

--- data/creation.sql
+++ data/creation.sql
@@ -1,2 +1,31 @@
-drop if exists users;
-drop if exists movies;
+create table users (
+	idUser INTEGER NOT NULL,
+	name varchar(100),
+	lastname varchar(100),
+	primary key("idUser")
+);
+
+create table account (
+	id INTEGER NOT NULL,
+	email varchar(100),
+	password varchar(255),
+	idUser1 INTEGER,	
+	idUser2 INTEGER,
+	idUser3 INTEGER,
+	idUser4 INTEGER,
+	idUser5 INTEGER,	
+	primary key ("id"),
+	foreign key ("idUser1") references users("idUser"),
+	foreign key ("idUser2") references users("idUser"),
+	foreign key ("idUser3") references users("idUser"),
+	foreign key ("idUser4") references users("idUser"),
+	foreign key ("idUser5") references users("idUser")
+);
+
+create table movies (
+	idMovie INTEGER NOT NULL,
+	title varchar(255),
+	description text,
+	movieUrl varchar(255),
+	primary key ("idMovie")
+);
