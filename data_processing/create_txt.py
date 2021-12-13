080abc4a7288e0b88de825445e5de47ba87e1d06
KevinThiboud <kevin.thiboud@etu.univ-lyon1.fr>
2021-12-13 20:49:02 +0100

    create parser

---
+++ data_processing/create_txt.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 13 15:03:00 2021
+
+@author: Kevin
+"""
+
+import sqlite3 
+con = sqlite3.connect('../data/IUTHub.db') #connexion a la base
+
+file = open('data.txt','w') #ouverture du fichier
+cur=con.cursor();
+for row in cur.execute('SELECT * from ranking'):
+    file.write((row))
+file.close()
